81217a8f5ad4d3528accbe6babd3395217f8d8d6
Michael Ballif <mballifcodes@gmail.com>
2022-11-16 00:37:45 -0700

    create all necessary functions, currently not working, need to debug with console.log

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let computerChoice;
+let computerSelection;
 function getComputerChoice(){
     let temp = Math.floor(Math.random()*10);
     if (temp >= 0 && temp <=2){
-        computerChoice = "rock";
+        computerSelection = "Rock";
         return;
     } else if (temp > 2 && temp < 6){
-        computerChoice = "paper";
+        computerSelection = "Paper";
         return;
     } else if (temp >= 6 && temp < 9){
-        computerChoice = "scissors";
+        computerSelection = "Scissors";
         return;
     } else {
         getComputerChoice();
@@ -18,17 +18,16 @@ function getComputerChoice(){
 }
 
 let playerSelection;
-
 function getPlayerSelection(){
-    temp = prompt("Do you choose rock, paper, or scissors?");
-    if (temp == "rock" || temp == "Rock") {
-        playerSelection = "rock";
+    temp = prompt("Do you choose rock, paper, or scissors?").toLowerCase();
+    if (temp == "rock") {
+        playerSelection = "Rock";
         return;
-    } else if (temp == "paper" || temp == "Paper") {
-        playerSelection = "paper";
+    } else if (temp == "paper") {
+        playerSelection = "Paper";
         return;
-    } else if (temp == "scissors" || temp == "Scissors") {
-        playerSelection = "scissors";
+    } else if (temp == "scissors") {
+        playerSelection = "Scissors";
         return;
     } else {
         console.log("That's not an option!  Try again:");
@@ -36,5 +35,71 @@ function getPlayerSelection(){
     }
 }
 
-getPlayerSelection();
-console.log(`You've selected ${playerSelection}!`);
+let winner;
+function getWinner(playerSelection, computerSelection) {
+    let temp;
+    switch (computerSelection) {
+        case "Rock":
+            switch (playerSelection) {
+                case "Rock":
+                    temp="tie";
+                    break;
+                case "Paper":
+                    temp="playerWin";
+                    break;
+                case "Scissors":
+                    temp="computerWin";
+                    break;
+            }
+            break;
+        case "Paper":
+            switch (playerSelection) {
+                case "Rock":
+                    temp="computerWin";
+                    break;
+                case "Paper":
+                    temp="tie";
+                    break;
+                case "Scissors":
+                    temp="playerWin"
+                    break;
+            }
+            break;
+        case "Scissors" :
+            switch (playerSelection) {
+                case "Rock":
+                    temp="playerWin";
+                    break;
+                case "Paper":
+                    temp="computerWin";
+                    break;
+                case "Scissors":
+                    temp="tie"
+                    break;
+            }
+    }
+    switch (temp) {
+        case "playerWin":
+            console.log(`You win! ${playerSelection} beats ${computerSelection.toLowerCase()}`);
+            break;
+        case "computerWin":
+            console.log(`You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`);
+            break;
+        case "tie":
+            console.log(`It was a tie! You both picked ${playerSelection.toLowerCase()}! :-0`)
+    }
+}
+
+function playAgainInquiry() {
+    let temp = (prompt("Would you like to play again? yes/no")).toLowerCase();
+    temp == "yes" ? playGame() : console.log("Okay, thanks for playing with me!");
+
+}
+
+function playGame(){
+    getPlayerSelection();
+    getComputerChoice();
+    getWinner();
+}
+
+playGame();
